5481bbdab1779f3cc736de82e246ba61cd3c60e0
Philipp Henkel <philipphenkel@users.noreply.github.com>
2020-12-15 16:39:33 +0100

    Remove function next

--- test.ts
+++ test.ts
@@ -3,6 +3,3 @@ makerbit.setDigitalPin(5, 1);
 makerbit.setAnalogPin(5, 1023);
 const level: number = makerbit.level(PinLevel.High);
 loops.forLoop(0, 5, (idx) => {});
-
-let a = 3;
-a = makerbit.next(a, 1, 5);
