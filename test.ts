8d67bfdaab1680dde9d2ce01eb9574d695e3a17c
Philipp Henkel <philipphenkel@users.noreply.github.com>
2021-07-26 07:38:11 +0200

    Add more tests

--- test.ts
+++ test.ts
@@ -2,4 +2,6 @@ makerbit.setLedPins(0);
 makerbit.setDigitalPin(5, 1);
 makerbit.setAnalogPin(5, 1023);
 const level: number = makerbit.level(PinLevel.High);
-loops.forLoop(0, 5, (idx) => {});
+loops.forLoop(0, 5, (idx) => { });
+makerbit.disableButtons();
+makerbit.enableButtons();
