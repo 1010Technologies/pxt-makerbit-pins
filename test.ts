d7640efaf83c438feb80d37603790daf45f4bcd4
Philipp Henkel <philipphenkel@users.noreply.github.com>
2021-07-26 19:30:41 +0200

    Switch to pxt-buttons

--- test.ts
+++ test.ts
@@ -3,5 +3,3 @@ makerbit.setDigitalPin(5, 1);
 makerbit.setAnalogPin(5, 1023);
 const level: number = makerbit.level(PinLevel.High);
 loops.forLoop(0, 5, (idx) => { });
-input.disableButtons();
-input.enableButtons();
